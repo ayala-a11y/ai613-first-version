02fa8dfaeac20f565f1fa7514376b0a100a1fd18
ayalak <ayalakubashka@gmail.com>
2025-11-16 17:57:56 +0200

    add TasksList

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 
 function App() {
+
+
+
+  
   return <>
-  <div className="App">
-    <h1>Welcome to React Router Dom</h1>
-  </div>
+  
   </>
    
   
---
+++ src/featurs/tasks/TasksList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const TasksList = () => {
+    const list = useSelector(state => state.tasks.todos);
+
+    return <>
+        <div>
+            <h2>Tasks List</h2>
+            {list.map((task) => (
+                <div key={task.id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+                    <h3>{task.title}</h3>
+                    <p>{task.desc}</p>
+                    <p>{task.date}</p>
+                    {task.img && <img src={task.img} style={{ maxWidth: "100px" }} />}
+                    <p>{task.isCompleted ? "Completed" : "Not completed"}</p>
+                </div>
+            ))}
+        </div>
+    </>
+};
+
+export default TasksList;
